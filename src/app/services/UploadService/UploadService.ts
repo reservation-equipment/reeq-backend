4b0961dd3a7eb072e2bce6cc96a233f900989bf5
Nekitech <nekitech-x@mail.ru>
2024-01-01 23:07:21 +0300

    fix

--- src/app/services/UploadService/UploadService.ts
+++ src/app/services/UploadService/UploadService.ts
@@ -1,13 +1,53 @@
-import {default as EasyYandexS3} from "easy-yandex-s3";
 import * as process from "process";
+import S3, {PutObjectRequest} from "aws-sdk/clients/s3";
 
 export class UploadService {
-    constructor() {
+    constructor(public yandexS3: any) {
     }
-    // async UploadImagesYandexObjectStorage(images: any) {
-    //     // console.log(images)
-    // }
+
+
+    async UploadImagesYandexS3(params: PutObjectRequest) {
+        try {
+            this.yandexS3.upload(params, (err: any, data: any) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`Image uploaded successfully. File URL: ${data.Location}`);
+                }
+            }); // Загрузка в бакет
+
+        } catch (e: any) {
+            console.log("Ошибка при загрузке в S3 storage: ", e, e.message)
+        }
+    }
+
+    async RemoveImageFromYandexS3(params: PutObjectRequest) {
+        try {
+            this.yandexS3.deleteObject(params, (err: any, data: any) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`Image deleted successfully. File URL: ${data.Location}`);
+                }
+            });
+
+        } catch (e: any) {
+            console.log("Ошибка при удалении в S3 storage: ", e, e.message)
+        }
+    }
 
 }
 
-export const uploadService = new UploadService()
+export const uploadService = new UploadService(
+    new S3({
+        endpoint: process.env.YANDEX_S3_ENDPOINT,
+        apiVersion: "latest",
+        accessKeyId: process.env.YANDEX_S3_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.YANDEX_S3_SECRET_KEY as string,
+        region: process.env.YANDEX_S3_REGION,
+        httpOptions: {
+            timeout: 10000,
+            connectTimeout: 10000,
+        },
+    })
+)
--- src/infrastructure/middlewares/UploadMiddleware.ts
+++ src/infrastructure/middlewares/UploadMiddleware.ts
@@ -1,11 +1,6 @@
-import multer from 'multer'
-import * as path from "path";
 import {prisma} from "../db/orm/prisma/PrismaClient";
 import {uploadService} from "../../app/services/UploadService/UploadService";
-import {default as EasyYandexS3} from "easy-yandex-s3";
-import process from "process";
-import S3 from 'aws-sdk/clients/s3';
-
+import {PutObjectRequest} from 'aws-sdk/clients/s3';
 
 
 // const storage = multer.diskStorage({
@@ -22,75 +17,44 @@ import S3 from 'aws-sdk/clients/s3';
 //     }
 // })
 // const upload = multer({ storage: storage })
-let s3 = new S3({
-    endpoint: 'https://storage.yandexcloud.net',
-    apiVersion: "latest",
-    accessKeyId: process.env.YANDEX_S3_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.YANDEX_S3_SECRET_KEY as string,
-    region: 'us-east-1',
-    httpOptions: {
-        timeout: 10000,
-        connectTimeout: 10000,
-    },
-});
-
-    // @ts-ignore
-    // let s3 = new EasyYandexS3({
-    //     auth: {
-    //         accessKeyId: process.env.YANDEX_S3_ACCESS_KEY_ID as string,
-    //         secretAccessKey: process.env.YANDEX_S3_SECRET_KEY as string
-    //     },
-    //     Bucket: "reeq-files",
-    //     debug: true,
-    //
-    // })
 
 
 export const UploadMiddleware = () => {
     return async (req: any, res: any, next: any) => {
-        // const {id} = req.query;
-        if(req.files) {
+        const {id} = req.query;
+        const fileNames: string[] = []
+        if (req.files) {
             for (let file of req.files) {
-                const {buffer,originalname } = file;
-                console.log(file)
-                const params = {
+                const {buffer, originalname} = file;
+                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+                const newName = `${uniqueSuffix}_${originalname}`
+
+                const params: PutObjectRequest = {
                     Bucket: 'reeq-files',
-                    Key: originalname,
+                    Key: `images/${newName}`,
                     Body: buffer,
                     ACL: 'public-read',
                     ContentType: "images/*",
 
                 }
-                try {
-                    let upload =  s3.upload(params, (err: any, data: any) => {
-                            if (err) {
-                                console.error(err);
-                            } else {
-                                console.log(`Image uploaded successfully. File URL: ${data.Location}`);
-                            }
-                        }); // Загрузка в бакет
-                    console.log(upload)
-                    res.send(upload);
-                } catch (e: any) {
-                    console.log("Ошибка при загрузке в S3 storage: ", e, e.message)
-                }
+
+                console.log(file)
+
+                await uploadService.UploadImagesYandexS3(params)
+                fileNames.push(newName)
+
             }
-            // await prisma.equipments.update({
-            //     where: {
-            //         id: Number(id)
-            //     },
-            //     data: {
-            //         img_hrefs: req.files.map((item: any) => item.filename)
-            //     }
-            // })
+            console.log(fileNames)
+            await prisma.equipments.update({
+                where: {
+                    id: Number(id)
+                },
+                data: {
+                    img_hrefs: fileNames
+                }
+            })
         }
-        // upload.array('image_equipment', 2)(req, res, (err: any) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return res.status(500).json({error: 'Unexpected end of form'});
-        //     }
-        //     next();
-        // });
+
         next();
     };
 };
